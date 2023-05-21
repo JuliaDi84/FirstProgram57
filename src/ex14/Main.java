dce5c0f88aa7b79d8a2650a16f50adba834b8846
JuliaDi84 <iuliadabija@gmail.com>
2023-05-21 12:14:14 +0300

    Am adaugat o linie

--- src/ex14/Main.java
+++ src/ex14/Main.java
@@ -15,6 +15,7 @@ public class Main {
 
             }
         }
+        System.out.println();
         printTable(tabel);
         Scanner scanner = new Scanner(System.in);
         while (true) {
